515f789e7b8b963491b2ca2af1d87651441ee7c6
Spencer Mogil <smogil12@gmail.com>
2020-03-28 17:29:25 -0400

    test: tailwind import into react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Tailwind from "./components/button";
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
-
+      <Tailwind />
       <h1>Tailwind Practice</h1>
     </div>
   );
---
+++ src/components/button.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Tailwind() {
+  return (
+    <span className="inline-flex rounded-md shadow-sm">
+      <button
+        type="button"
+        className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+      >
+        Button text
+      </button>
+    </span>
+  );
+}
+
+export default Tailwind;
