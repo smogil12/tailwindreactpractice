b5220961d80c25f228ae0910eaaca08b4c9c10fc
Spencer Mogil <smogil12@gmail.com>
2020-03-28 16:54:06 -0400

    chore: create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <h1>Tailwind Practice</h1>
+    </div>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="card">
+      <div className="card-body">This is some text within a card body.</div>
+    </div>
+  );
+}
+
+export default NavBar;
